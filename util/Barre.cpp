d1988bede1755f3d9247c08e43f935614a4f36ff
benlamouche <benlamouche78@hotmail.fr>
2019-06-12 19:56:45 +0200

    maj barre

--- util/Barre.cpp
+++ util/Barre.cpp
@@ -1,4 +1,5 @@
 #include "Barre.h"
+#include "fonctionDessin.h"
 SDL_Rect Barre::position={0,0,0,0};
 
 Barre::Barre(int &val,int &valmax): m_val(val),m_valmax(valmax)
@@ -8,6 +9,8 @@ Barre::Barre(int &val,int &valmax): m_val(val),m_valmax(valmax)
     m_largeur=10;
     m_largeurBord=1;
     m_longueur=100;
+    bR=bG=bB=0;
+    fR=fG=fB=200;
 }
 
 Barre::~Barre()
@@ -21,9 +24,9 @@ void Barre::afficheGD(SDL_Window *ecran)
             bar=(m_val*m_longueur)/m_valmax;
             if (bar<=0)bar=0;
             barre=SDL_CreateRGBSurface(0,bar,m_largeur,32,0,0,0,0);
-            SDL_FillRect(barre,NULL,SDL_MapRGB(barre->format,200,200,200));
+            SDL_FillRect(barre,NULL,SDL_MapRGB(barre->format,fR,fG,fB));
             barreMax=SDL_CreateRGBSurface(0,m_longueur+m_largeurBord*2,m_largeur+m_largeurBord*2,32,0,0,0,0);
-            SDL_FillRect(barreMax,NULL,SDL_MapRGB(barreMax->format,0,0,0));
+            SDL_FillRect(barreMax,NULL,SDL_MapRGB(barreMax->format,bR,bG,bB));
 
 
             position.x=posBarX;
@@ -43,9 +46,9 @@ void Barre::afficheDG(SDL_Window *ecran)
             bar=(m_val*m_longueur)/m_valmax;
             if (bar<=0)bar=0;
             barre=SDL_CreateRGBSurface(0,bar,m_largeur,32,0,0,0,0);
-            SDL_FillRect(barre,NULL,SDL_MapRGB(barre->format,200,200,200));
+            SDL_FillRect(barre,NULL,SDL_MapRGB(barre->format,fR,fG,fB));
             barreMax=SDL_CreateRGBSurface(0,m_longueur+m_largeurBord*2,m_largeur+m_largeurBord*2,32,0,0,0,0);
-            SDL_FillRect(barreMax,NULL,SDL_MapRGB(barreMax->format,0,0,0));
+            SDL_FillRect(barreMax,NULL,SDL_MapRGB(barreMax->format,bR,bG,bB));
 
 
             position.x=posBarX;
@@ -66,9 +69,9 @@ void Barre::afficheHB(SDL_Window *ecran)
             bar=(m_val*m_longueur)/m_valmax;
             if (bar<=0)bar=0;
             barre=SDL_CreateRGBSurface(0,m_largeur,bar,32,0,0,0,0);
-            SDL_FillRect(barre,NULL,SDL_MapRGB(barre->format,200,200,200));
+            SDL_FillRect(barre,NULL,SDL_MapRGB(barre->format,fR,fG,fB));
             barreMax=SDL_CreateRGBSurface(0,m_largeur+m_largeurBord*2,m_longueur+m_largeurBord*2,32,0,0,0,0);
-            SDL_FillRect(barreMax,NULL,SDL_MapRGB(barreMax->format,0,0,0));
+            SDL_FillRect(barreMax,NULL,SDL_MapRGB(barreMax->format,bR,bG,bB));
 
 
             position.x=posBarX;
@@ -88,9 +91,9 @@ void Barre::afficheBH(SDL_Window *ecran)
             bar=(m_val*m_longueur)/m_valmax;
             if (bar<=0)bar=0;
             barre=SDL_CreateRGBSurface(0,m_largeur,bar,32,0,0,0,0);
-            SDL_FillRect(barre,NULL,SDL_MapRGB(barre->format,200,200,200));
+            SDL_FillRect(barre,NULL,SDL_MapRGB(barre->format,fR,fG,fB));
             barreMax=SDL_CreateRGBSurface(0,m_largeur+m_largeurBord*2,m_longueur+m_largeurBord*2,32,0,0,0,0);
-            SDL_FillRect(barreMax,NULL,SDL_MapRGB(barreMax->format,0,0,0));
+            SDL_FillRect(barreMax,NULL,SDL_MapRGB(barreMax->format,bR,bG,bB));
 
 
             position.x=posBarX;
@@ -109,10 +112,41 @@ void Barre::afficheGDInv(SDL_Window *ecran)
             bar=((m_valmax-m_val)*m_longueur)/m_valmax;
             if (bar<=0)bar=0;
             barre=SDL_CreateRGBSurface(0,bar,m_largeur,32,0,0,0,0);
-            SDL_FillRect(barre,NULL,SDL_MapRGB(barre->format,200,200,200));
+            SDL_FillRect(barre,NULL,SDL_MapRGB(barre->format,fR,fG,fB));
             barreMax=SDL_CreateRGBSurface(0,m_longueur+m_largeurBord*2,m_largeur+m_largeurBord*2,32,0,0,0,0);
-            SDL_FillRect(barreMax,NULL,SDL_MapRGB(barreMax->format,0,0,0));
+            SDL_FillRect(barreMax,NULL,SDL_MapRGB(barreMax->format,bR,bG,bB));
 
+
+            position.x=posBarX;
+            position.y=posBarY;
+            SDL_UpperBlit(barreMax,NULL,SDL_GetWindowSurface(ecran),&position);
+            position.x=posBarX+m_largeurBord;
+            position.y=posBarY+m_largeurBord;
+            SDL_UpperBlit(barre,NULL,SDL_GetWindowSurface(ecran),&position);
+
+            SDL_FreeSurface(barre);
+            SDL_FreeSurface(barreMax);
+}
+
+void Barre::draw(SDL_Window *ecran)
+{
+
+            bar=(m_val*m_longueur)/m_valmax;
+            if (bar<=0)bar=0;
+            barre=SDL_CreateRGBSurface(0,bar,m_largeur,32,0,0,0,0);
+
+
+
+            SDL_FillRect(barre,NULL,SDL_MapRGB(barre->format,fR,fG,fB));
+// truc tré important de ouf !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            SDL_Surface *barreMax=SDL_CreateRGBSurfaceWithFormat(0, m_longueur+m_largeurBord*2,m_largeur+m_largeurBord*2, 32, SDL_PIXELFORMAT_RGBA32);
+            //SDL_SetSurfaceBlendMode(barreMax,SDL_BLENDMODE_BLEND);
+
+            SDL_FillRect(barreMax,NULL,SDL_MapRGBA(barreMax->format,255,0,0,0));
+            ligneHorizontale(barreMax,1,0,m_longueur+m_largeurBord*2-2,SDL_MapRGB(barreMax->format,255,255,255));
+            ligneHorizontale(barreMax,1,m_largeur+m_largeurBord*2-1,m_longueur+m_largeurBord*2-2,SDL_MapRGB(barreMax->format,255,255,255));
+            ligneVerticale(barreMax,0,1,m_largeur+m_largeurBord*2-2,SDL_MapRGB(barreMax->format,255,255,255));
+            ligneVerticale(barreMax,m_longueur+m_largeurBord*2-1,1,m_largeur+m_largeurBord*2-2,SDL_MapRGB(barreMax->format,255,255,255));
 
             position.x=posBarX;
             position.y=posBarY;
--- util/Barre.h
+++ util/Barre.h
@@ -6,15 +6,10 @@
 #include <SDL_image.h>
 #include <SDL_ttf.h>
 
-
-
-
 /* sert a afficher des barre (cooldown barre de vie) */
-
 
 class Barre
 {
-
     public:
         Barre(int &val,int &valmax);
 
@@ -27,12 +22,27 @@ class Barre
         void setPosition(int x,int y){posBarX=x,posBarY=y;}
         void setLLL(int longueur=100,int largeur=15,int largeurBord=1)
         {m_longueur=longueur;m_largeur=largeur;m_largeurBord=largeurBord;}
+        void setColor(int R,int G,int B)
+        {
+            fR=R;
+            fG=G;
+            fB=B;
+        }
+
+        void setBackColor(int R,int G,int B)
+        {
+            bR=R;
+            bG=G;
+            bB=B;
+        }
 
         void afficheGD(SDL_Window *ecran);//  affiche Gauche Droite
         void afficheDG(SDL_Window *ecran);
         void afficheHB(SDL_Window *ecran);
         void afficheBH(SDL_Window *ecran);
         void afficheGDInv(SDL_Window *ecran);//  Variante pour les cooldown barre pleine quand cooldown = 0
+
+        void draw(SDL_Window *ecran);
 
     protected:
 
@@ -46,6 +56,8 @@ class Barre
         SDL_Surface *barreMax;
         int bar,posBarX,posBarY;
         static SDL_Rect position;
+        //color RGB
+        int fR,fG,fB,bR,bG,bB;
 
 };
 //unsigned int Barre::m_largeurBord=1;
