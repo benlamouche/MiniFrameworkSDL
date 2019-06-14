9a2e1989d1a486e693d2b9fe469056ce168ca5a6
benlamouche <benlamouche78@hotmail.fr>
2019-06-14 14:04:04 +0200

    ajout collisions segment

--- util/collision.cpp
+++ util/collision.cpp
@@ -18,3 +18,36 @@ bool boxCollision(SDL_Rect box1,SDL_Rect box2){
    else
           return true;
 }
+
+bool CollisionDroiteSeg(SDL_Point A,SDL_Point B,SDL_Point O,SDL_Point P)
+{
+  SDL_Point AO,AP,AB;
+  AB.x = B.x - A.x;
+  AB.y = B.y - A.y;
+  AP.x = P.x - A.x;
+  AP.y = P.y - A.y;
+  AO.x = O.x - A.x;
+  AO.y = O.y - A.y;
+  if ((AB.x*AP.y - AB.y*AP.x)*(AB.x*AO.y - AB.y*AO.x)<0)
+     return true;
+  else
+     return false;
+}
+
+bool CollisionSegSeg(SDL_Point A,SDL_Point B,SDL_Point O,SDL_Point P)
+{
+  if (CollisionDroiteSeg(A,B,O,P)==false)
+     return false;  // inutile d'aller plus loin si le segment [OP] ne touche pas la droite (AB)
+  if (CollisionDroiteSeg(O,P,A,B)==false)
+     return false;
+  return true;
+}
+
+bool CollisionSegSeg(int Ax ,int Ay ,int Bx ,int By,int Ox ,int Oy,int Px ,int Py)
+{
+    SDL_Point A = {Ax,Ay};
+    SDL_Point B = {Bx,By};
+    SDL_Point O = {Ox,Oy};
+    SDL_Point P = {Px,Py};
+    return CollisionSegSeg(A,B,O,P);
+}
--- util/collision.h
+++ util/collision.h
@@ -5,6 +5,8 @@
 
 bool pointCollision(int x,int y,SDL_Rect box);
 bool boxCollision(SDL_Rect box1,SDL_Rect box2);
+bool CollisionSegSeg(SDL_Point A,SDL_Point B,SDL_Point O,SDL_Point P);
+bool CollisionSegSeg(int Ax ,int Ay ,int Bx ,int By,int Ox ,int Oy,int Px ,int Py);
 
 
 #endif // COLLISION_H_INCLUDED
